e4fe4bc495bc841e6c872de2d38ef09cb1479980
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-12 09:31:11 +0100

    Create CustomSwitch.jsx

---
+++ frontend/src/components/switch/CustomSwitch.jsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import Cart from '../assets/Cart.svg';
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 130,
+    height: 55,
+    padding: 5,
+
+    '& .MuiSwitch-switchBase': {
+      margin: 0,
+      padding: 0 ,
+      transform: 'translateX(0px)',
+      '&.Mui-checked': {
+
+        color: '#fff',
+        transform: 'translateX(70px)',
+        '& .MuiSwitch-thumb:before': {
+          
+
+          backgroundImage: `url(${Cart})`,
+        },
+        
+        '& + .MuiSwitch-track': {
+
+          opacity: 1,
+          backgroundColor: theme.palette.mode === 'dark' ? '#FFFFFF' : '#FFFFFF',
+          border: '2px solid #FFFFFF'
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      width: 55,
+      height: 55,
+      '&:before': {
+        content: "''",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url('${Cart}')`,
+        border: '1px solid rgba(2, 2, 2, 0.74)',
+        borderRadius: 45 ,
+      },
+    },
+    '& .MuiSwitch-track': {
+      '&:after':{
+        content: "''"}
+        ,
+        '&:before':{
+          content: "'_____Switch to buy'"}
+          ,
+      opacity: 1,
+      backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#FFFFFF',
+      borderRadius: 53,
+      
+    },
+    
+    
+  }));
+
+export const CustomSwitch = () => {
+  return (
+    <>
+      <FormGroup>
+      <FormControlLabel
+        control={<MaterialUISwitch sx={{ m: 1 }} />}
+        
+      />  
+      </FormGroup>
+    </>
+    
+  )
+}
