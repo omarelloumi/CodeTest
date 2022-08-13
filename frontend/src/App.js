ccba7bf140bdd9a866b62f126525455be4d0a7ef
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-13 15:17:28 +0100

    Remove unnecessary grid form app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Body from "./pages/Body/Body";
-import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <>
       <Navbar />
       <Body />
-      <Grid item xs={12}>
-        <div style={{ height: "2rem" }}></div>
-      </Grid>
     </>
   );
 }
